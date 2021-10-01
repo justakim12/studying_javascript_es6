da12ea2f6a76cb26748bfc42424d1a6e63285ff7
David K. Kim <rlaruddlf711@naver.com>
2021-10-01 11:40:37 +0900

    add template literals

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 let word1 = 'Dylan';
 let word2 = 'Israel';
 
-const fullName = word1 + '' + word2;
+const fullName = `${word1} ${word2}`
 
 console.log(fullName);
+
+// Multiline String
+let example = `${word1}
+${word2}
+`;
+
+console.log(example)
